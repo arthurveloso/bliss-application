ca8a088ccced3e88e9cad938028da9bcfdf9eef5
Arthur Veloso Gouveia de Melo <arthur.gouveia-melo@itau-unibanco.com.br>
2020-12-23 17:34:53 -0400

    Empty project created, removed .storyboard files

---
+++ bliss-app/bliss-app/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  bliss-app
+//
+//  Created by Arthur Veloso Gouveia Melo on 23/12/20.
+//  Copyright © 2020 Arthur Veloso Gouveia Melo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ bliss-app/bliss-app/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  bliss-app
+//
+//  Created by Arthur Veloso Gouveia Melo on 23/12/20.
+//  Copyright © 2020 Arthur Veloso Gouveia Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
