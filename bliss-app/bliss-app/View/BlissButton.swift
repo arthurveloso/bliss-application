3553533cb2c761c4113a64feb66a6b6b8215da9f
Arthur Veloso Gouveia de Melo <arthur.gouveia-melo@itau-unibanco.com.br>
2020-12-24 15:14:35 -0400

    Finished to build the v0 of Home layout

---
+++ bliss-app/bliss-app/View/BlissButton.swift
@@ -0,0 +1,27 @@
+//
+//  BlissButton.swift
+//  bliss-app
+//
+//  Created by Arthur Veloso Gouveia Melo on 24/12/20.
+//  Copyright © 2020 Arthur Veloso Gouveia Melo. All rights reserved.
+//
+
+import UIKit
+
+class BlissButton: UIButton {
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        setTitle(title.uppercased(), for: .normal)
+        setupStyle()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupStyle() {
+        self.backgroundColor = .purple
+        self.layer.cornerRadius = 5
+    }
+}
--- bliss-app/bliss-app/View/HomeView.swift
+++ bliss-app/bliss-app/View/HomeView.swift
@@ -23,7 +23,46 @@ class HomeView: UIView {
     lazy var buttonsStack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
+        stack.spacing = 10
         return stack
+    }()
+    
+    lazy var randomEmojiButton: BlissButton = {
+        let button = BlissButton(title: "Random Emoji")
+        return button
+    }()
+    
+    lazy var emojisListButton: BlissButton = {
+        let button = BlissButton(title: "Emojis List")
+        return button
+    }()
+    
+    lazy var searchStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 10
+        return stack
+    }()
+    
+    lazy var searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.barTintColor = .purple
+        return bar
+    }()
+    
+    lazy var searchButton: BlissButton = {
+        let button = BlissButton(title: "Search")
+        return button
+    }()
+    
+    lazy var avatarsListButton: BlissButton = {
+        let button = BlissButton(title: "Avatars List")
+        return button
+    }()
+    
+    lazy var appleReposButton: BlissButton = {
+        let button = BlissButton(title: "Apple Repos")
+        return button
     }()
     
     private weak var delegate: HomeViewDelegate?
@@ -31,17 +70,35 @@ class HomeView: UIView {
     convenience init(delegate: HomeViewDelegate? = nil) {
         self.init(frame: .zero)
         self.delegate = delegate
+        backgroundColor = .white
         setupSubviews()
     }
     
     func setupSubviews() {
-        backgroundColor = .white
-        
+        searchStack.addArrangedSubview(searchBar)
+        searchStack.addArrangedSubview(searchButton)
+
+        buttonsStack.addArrangedSubview(randomEmojiButton)
+        buttonsStack.addArrangedSubview(emojisListButton)
+        buttonsStack.addArrangedSubview(searchStack)
+        buttonsStack.addArrangedSubview(avatarsListButton)
+        buttonsStack.addArrangedSubview(appleReposButton)
+
         addSubview(emojiImage)
         emojiImage.snp.makeConstraints { (make) in
             make.top.equalToSuperview().inset(150)
             make.centerX.equalToSuperview()
             make.width.height.equalTo(150)
+        }
+
+        addSubview(buttonsStack)
+        buttonsStack.snp.makeConstraints { (make) in
+            make.top.equalTo(emojiImage.snp.bottom).offset(40)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        searchButton.snp.makeConstraints { (make) in
+            make.width.equalTo(buttonsStack.snp.width).multipliedBy(0.3)
         }
     }
 }
